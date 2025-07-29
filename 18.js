4181721f37a04c822ec894b102e151dbbf308682
Sebastian <winigossos@gmail.com>
2025-07-29 09:34:45 -0300

    feat: 19.js resolved

--- 18.js
+++ 18.js
@@ -9,18 +9,29 @@
 
 
 export default function fixFiles(files) {
- // ¡No olvides compartir tu solución en redes!
-  return []
+  const filesRepetead = {}
+  let result = []
+
+  for ( let f of files ) {
+    if( filesRepetead[f] >= 0 ) filesRepetead[f]++
+    else filesRepetead[f] = 0
+
+  
+    if( filesRepetead[f] === 0 ) result.push(f)
+    else result.push(`${f}(${filesRepetead[f]})`)
+  }
+  
+  return result
 }
 
-const files = ['photo', 'postcard', 'photo', 'photo', 'video']
-fixFiles(files) // ['photo', 'postcard', 'photo(1)', 'photo(2)', 'video']
+// const files = ['photo', 'postcard', 'photo', 'photo', 'video']
+// console.log(fixFiles(files)) // ['photo', 'postcard', 'photo(1)', 'photo(2)', 'video']
 
-const files2 = ['file', 'file', 'file', 'game', 'game']
-fixFiles(files2) = ['file', 'file(1)', 'file(2)', 'game', 'game(1)']
+// const files2 = ['file', 'file', 'file', 'game', 'game']
+// console.log(fixFiles(files2)) // ['file', 'file(1)', 'file(2)', 'game', 'game(1)']
 
 // ojo que los elfos ya tenían archivos con (1)... ¡y pueden estar repetidos!
 const files3 = ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)']
-fixFiles(files3) // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
+console.log(fixFiles(files3)) // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
 
 // Por cierto, nos han dicho que son Agile y usan Scrum. Por eso quieren saber cuánto tiempo vas a tardar para saber cuándo van a poder usarlo. Que hay prisa. 😝 Así que entra a Discord y cuéntanos.
