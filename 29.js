12e078440f6cc844598fee23e6fe17a19770aacb
Sebastian <winigossos@gmail.com>
2025-08-03 11:49:57 -0300

    feat: 29.js resolved

--- 29.js
+++ 29.js
@@ -5,35 +5,48 @@
 // Cada caja representa sus medidas con un objeto. Por ejemplo: {l: 2, w: 3, h: 2}. Esto significa que la caja tiene una longitud de 2, una anchura de 3 y una altura de 2.
 
 function fitsInOneBox(boxes) {
-  return false
+  let lastH = Infinity
+  let lastW = Infinity
+  let lastL = Infinity
+
+  boxes.sort((a,b) => (b.l - a.l) || (b.w - a.w) || (b.h - a.h)  )
+  
+  for( let box of boxes ) {
+    const isLessThan = (box.l < lastL) && (box.h < lastH) && (box.w < lastW) 
+    if(!isLessThan) return false
+    lastL = box.l;
+    lastH = box.h;
+    lastW = box.w;
+  }
+  return true
 }
 
 // Una caja entra en otra caja si todos los lados de la primera son menores a los lados de la segunda. Los elfos nos han dicho que las cajas no se pueden rotar, así que no se puede poner una caja de 2x3x2 en una caja de 3x2x2. Veamos unos ejemplos:
 
-// fitsInOneBox([
-//   { l: 1, w: 1, h: 1 },
-//   { l: 2, w: 2, h: 2 }
-// ]) // true
+console.log(fitsInOneBox([
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 }
+])) // true
 // En el ejemplo anterior, la caja más pequeña entra en la caja más grande. Por lo tanto, es posible empaquetar todas las cajas en una sola. Ahora veamos un caso que no:
 
-// const boxes = [
-//   { l: 1, w: 1, h: 1 },
-//   { l: 2, w: 2, h: 2 },
-//   { l: 3, w: 1, h: 3 }
-// ]
+const boxes = [
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 }
+]
 
-// fitsInOneBox(boxes) // false
+console.log(fitsInOneBox(boxes)) // false
 // En el ejemplo anterior, la caja más pequeña entra en la caja del medio, pero la caja del medio no entra en la caja más grande. Por lo tanto, no es posible empaquetar todas las cajas en una sola.
 
 // Ten en cuenta que las cajas pueden no venir en orden:
 
-// const boxes = [
-//   { l: 1, w: 1, h: 1 },
-//   { l: 3, w: 3, h: 3 },
-//   { l: 2, w: 2, h: 2 }
-// ]
+const boxes2 = [
+  { l: 1, w: 1, h: 1 },
+  { l: 3, w: 3, h: 3 },
+  { l: 2, w: 2, h: 2 }
+]
 
-// fitsInOneBox(boxes) // true
+console.log(fitsInOneBox(boxes2)) // true
 // En el ejemplo anterior, la primer caja cabe en la tercera, y la tercera en la segunda. Por lo tanto, es posible empaquetar todas las cajas en una sola.
 
 // Cosas a tener en cuenta:
